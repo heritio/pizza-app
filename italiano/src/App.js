e601a25c2d23c08522507390054293cd4c22a093
Heritier Akilimali <herri_93@hotmail.com>
2021-05-05 20:27:12 +0200

    Add Google fonts

--- italiano/src/App.js
+++ italiano/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3{
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
-  return <div>Hello italiano</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div>Hello italiano</div>
+    </>
+  );
 }
 
 export default App;
