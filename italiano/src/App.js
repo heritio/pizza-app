bb4eaeb74575b1361ef9b261301c1e76a7ca5e69
Heritier Akilimali <herri_93@hotmail.com>
2021-05-05 21:00:00 +0200

    Add Navbar, color and title style

--- italiano/src/App.js
+++ italiano/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import { Navbar } from "./Navbar/Navbar";
+
 const GlobalStyle = createGlobalStyle`
   body {
+    margin: 0;
+    
     font-family: 'Open Sans', sans-serif;
   }
   h1,h2,h3{
@@ -13,6 +17,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <Navbar />
       <div>Hello italiano</div>
     </>
   );
